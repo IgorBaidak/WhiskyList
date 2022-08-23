83e73c51723e78e306375c7bc35a2aa889c39da1
IgorBaidak <igorbaidak@gmail.com>
2022-08-23 09:46:01 +0300

    Start

---
+++ WhiskyList/WhiskyList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhiskyList
+//
+//  Created by Igor Baidak on 23.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
